c6a296f38c1078c1e73ce40d5bfe715770eba212
kamaaldiin <kamaaldiin51@gmail.com>
2021-11-08 08:13:17 +0300

    my first code

---
+++ km.js
@@ -0,0 +1,78 @@
+// barashada js 
+// intordocution to js
+/* console.log("kamaaldiin");
+console.log('kamaaldiin');
+
+let age = 45;
+let year  = 2021;
+console.log(age, year);
+age = 66;
+console.log(age);
+
+const kmk = 77;
+console.log(kmk);
+ kmk =88;
+console.log(kmk);
+
+console.log('kamaaldiin');
+console.log(68); */
+// string   
+/*console.log('kamaaldiin');
+let email = 'kamaaldiin@gmail.com';
+console.log(email);
+let fullName= 'muxsin yuusuf';
+console.log(fullName);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+console.log(fullName.toLocaleLowerCase());
+console.log(fullName.charAt(5)); */
+// methedes 
+//let kl = "kamaaldiin";
+//let result= kl.lastIndexOf('d');
+//let result=kl.slice('0','6');
+//let result=kl.substr('6');
+//let result= kl.replace('kamaaldiin','yusuf');
+//console.log(result);
+//numbers 
+//let you =10;
+//const pi = 3.14;
+//onsole.log(you,pi);
+//console.log(10/2);
+//let result = you % 6;
+//let result = pi * you **2;
+//let result = 6 *(10+3)**2;
+//console.log(result);
+
+//let kr = 4;
+ //r = kr+6;
+//kr++;
+//kr--;
+//kr +=4;
+//console.log(kr);
+//let result = 'kamaaldiin '  + kr +  ' waa'  + ' xaasle';
+//console.log(result);
+ /*const  attitude = 'the best player 2021';
+ const player = ' messi';
+
+ let result = 'the winner  will be ' + attitude +' is' + player  ;
+ console.log(result);*/
+ // undifined 
+ /*let undi;
+ console.log(undi);
+ // null
+ let nll= 'you';
+ console.log(nll);*/
+ //let booleans = 'kamaaldiin 8 & 83uherfj';
+//let result=booleans.includes('k');
+//let result= booleans.includes('b');
+let nm='479';
+nm=Number(nm);
+console.log( nm +1);
+console.log(typeof nm);
+// > < <= >=
+let kaweyn = 45;
+console.log(45!=45);
+let kayar=25;
+console.log(kayar===25) ;
+//console.log(result);
+
